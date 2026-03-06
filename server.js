69a95dfa3312f5ff35a89537a3ee2f225caefbf1
kibuukahassan12-code <kibuukahassan12@gmail.com>
2026-03-06 23:23:27 +0300

    Production routing fix

--- server.js
+++ server.js
@@ -2,3 +2,7 @@ const path=require('path');
 import './server/index.js';
 app.use(express.static(path.join(__dirname,'dist')));
 app.get('*',(req,res)=>res.sendFile(path.join(__dirname,'dist','index.html')));
+
+const path=require("path");
+app.use(express.static(path.join(__dirname,"dist")));
+app.get("*",(req,res)=>res.sendFile(path.join(__dirname,"dist","index.html")));
