4a730e911e63d2ebfd03362ad08c0b70f08d9a80
kibuukahassan12-code <kibuukahassan12@gmail.com>
2026-03-06 23:13:36 +0300

    Configure server to serve built static files

--- server.js
+++ server.js
@@ -1,1 +1,4 @@
+const path=require('path');
 import './server/index.js';
+app.use(express.static(path.join(__dirname,'dist')));
+app.get('*',(req,res)=>res.sendFile(path.join(__dirname,'dist','index.html')));
